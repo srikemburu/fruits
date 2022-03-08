dac572e024e45d21f807ef749798b497af37753e
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-08 15:32:54 -0500

    commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next)=>{
   next()
 })
 //keep this near the top 
-app.use(express.urlencoded({extened:false}))
+app.use(express.urlencoded({extended:true}))
 
 //set up view engine above routes
 app.set('view engine', 'jsx')
